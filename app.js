625141f2122d71cf33690f7ec40e16865af76afd
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-14 22:55:43 +0530

    calling mapbox api for geocoding

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const request = require('request');
 const chalk = require('chalk');
 const config = require('./config');
 
-const url = `https://api.darksky.net/forecast/${config.DARKSKY_SECRET_KEY}/17.387140,78.491684?units=si`;
+const darksky_forecast_url = `https://api.darksky.net/forecast/${config.DARKSKY_SECRET_KEY}/17.387140,78.491684?units=si`;
+const mapbox_geocoding_url= `https://api.mapbox.com/geocoding/v5/mapbox.places/hyderabad.json?access_token=${config.MAPBOX_ACCESS_TOKEN}&limit=1`;
 
-request({url, json: true},(error, response, body)=>{
-    printForecast(body.currently);
+// request({url: darksky_forecast_url, json: true},(error, response, body)=>{
+//     printForecast(body.currently);
+// })
+
+request({url: mapbox_geocoding_url,json:true},(error, response, body)=>{
+    console.log(`Latitude: ${getChalkInfoDecoration(body.features[0].center[0])} & Longitude: ${getChalkInfoDecoration(body.features[0].center[1])}`);
 })
 
 const printForecast = (currently) =>{
