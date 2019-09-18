fcffbaecfd7c8d0bd58c2e4c36c4617e2e928527
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-18 12:36:55 +0530

    written weather api for getting forecasts

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const location =require('./location');
+const weather = require('./weather');
 
 const app = express();
 
@@ -17,6 +19,29 @@ app.get('/about',(req,res)=>{
 app.get('/help', (req,res)=>{
     res.send({
         component: 'help',
+    })
+})
+
+app.get('/weather',(req, res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Address Parameter is mandatory!'
+        })
+    }
+    location.geocode(req.query.address,(error, geocodeResponse)=>{
+        if(error){
+            return res.send({
+                error
+            })
+        }
+        weather.forecast(geocodeResponse,(error,response)=>{
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+            res.send(response)
+        })
     })
 })
 
